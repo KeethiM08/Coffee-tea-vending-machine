07f850998303949eaf63ee65edebbf1d855ca8e6
Your Name <you@example.com>
2024-06-03 15:57:27 +0530

    option select

---
+++ select.h
@@ -0,0 +1,11 @@
+#ifndef VENDING_MACHINE_H_INCLUDED
+#define VENDING_MACHINE_H_INCLUDED
+
+#include <stdio.h>
+float selectHotCoffee();
+float selectColdCoffee() ;
+float selectTea();
+float selectCookie();
+
+#endif
+
