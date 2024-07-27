33f4e82c3a0d7ff4448a704454ef8437070fc36c
Keerthi M <168397418+KeethiM08@users.noreply.github.com>
2024-07-27 19:29:23 +0530

    Update hotcoffee.c

--- hotcoffee.c
+++ hotcoffee.c
@@ -1,34 +1,33 @@
 #include "select.h"
 
-float selectHotCoffee() {
-    printf("\nTypes of Hot Coffee:\n");
-    printf("1. Cappuccino coffee (Rs.230.00)\n");
-    printf("2. Filter coffee (Rs.55.50)\n");
-    printf("3. Latte coffee (Rs.280.40)\n");
-    printf("4. Americano coffee (Rs.300.75)\n");
+  printf("\nTypes of Hot Coffee:\n");
+    printf("1. Cappuccino coffee (Rs.70.00)\n");
+    printf("2. Filter coffee (Rs.30.00)\n");
+    printf("3. Latte coffee (Rs.85.00)\n");
+    printf("4. Americano coffee (Rs.100.00)\n");
     printf("5. Cancel\n");
     int coffee_choice;
-    printf("Enter your choice (1, 2, 3, 4, or 5): ");
+    printf("Enter your choice (1, 2, 3, 4 or 5): ");
     scanf("%d", &coffee_choice);
 
     switch (coffee_choice) {
         case 1:
-            printf("You've selected Cappuccino coffee. Price: Rs.230.00\n");
-            return 230.00;
+            printf("You've selected Cappuccino coffee. Price: Rs.70.00\n");
+            return 70.00;
         case 2:
-            printf("You've selected Filter coffee. Price: Rs.55.50\n");
-            return 55.50;
+            printf("You've selected Filter coffee. Price: Rs.30.00\n");
+            return 30.00;
         case 3:
-            printf("You've selected Latte coffee. Price: Rs.280.40\n");
-            return 280.40;
+            printf("You've selected Latte coffee. Price: Rs.85.00\n");
+            return 85.00;
         case 4:
-            printf("You've selected Americano coffee. Price: Rs.300.75\n");
-            return 300.75;
+            printf("You've selected Americano coffee. Price: Rs.100.00\n");
+            return 100.00;
         case 5:
             printf("You've cancelled the selection.\n");
-            return -1.0;
+            return 0.0;
         default:
             printf("Invalid choice. Assuming no coffee.\n");
-            return -1.0;
+            return 0.0;
     }
 }
